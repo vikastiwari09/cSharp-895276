877f6fbc873f56581337ce6b3b46c9152cb822a2
theVikasTiwari <75440329+vikastiwari09@users.noreply.github.com>
2024-02-13 18:46:06 -0500

    Add files via upload

---
+++ assignment2/assignment2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Interval
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a real number: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        bool belongsToI = (x >= 2 && x < 3) || (x >= 0 && x <= 1) || (x >= -10 && x <= -2);
+
+        Console.WriteLine($"x {(belongsToI ? "belongs to" : "does not belong to")} I");
+    }
+}
+
